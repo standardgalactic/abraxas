5d169a9e379041bc34760e6e60f34ea1162fab57
standardgalactic <standardgalactic@protonmail.com>
2024-12-30 22:20:48 -0400

    Alignment Tree

---
+++ my-bashrc.sh
@@ -0,0 +1,416 @@
+# ~/.bashrc: executed by bash(1) for non-login shells.
+# see /usr/share/doc/bash/examples/startup-files (in the package bash-doc)
+# for examples
+
+# If not running interactively, don't do anything
+case $- in
+    *i*) ;;
+      *) return;;
+esac
+
+# don't put duplicate lines or lines starting with space in the history.
+# See bash(1) for more options
+HISTCONTROL=ignoreboth
+
+# append to the history file, don't overwrite it
+shopt -s histappend
+
+# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
+HISTSIZE=1000
+HISTFILESIZE=2000
+
+# check the window size after each command and, if necessary,
+# update the values of LINES and COLUMNS.
+shopt -s checkwinsize
+
+# If set, the pattern "**" used in a pathname expansion context will
+# match all files and zero or more directories and subdirectories.
+#shopt -s globstar
+
+# make less more friendly for non-text input files, see lesspipe(1)
+[ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
+
+# set variable identifying the chroot you work in (used in the prompt below)
+if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
+    debian_chroot=$(cat /etc/debian_chroot)
+fi
+
+# set a fancy prompt (non-color, unless we know we "want" color)
+case "$TERM" in
+    xterm-color|*-256color) color_prompt=yes;;
+esac
+
+# uncomment for a colored prompt, if the terminal has the capability; turned
+# off by default to not distract the user: the focus in a terminal window
+# should be on the output of commands, not on the prompt
+#force_color_prompt=yes
+
+if [ -n "$force_color_prompt" ]; then
+    if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
+    # We have color support; assume it's compliant with Ecma-48
+    # (ISO/IEC-6429). (Lack of such support is extremely rare, and such
+    # a case would tend to support setf rather than setaf.)
+    color_prompt=yes
+    else
+    color_prompt=
+    fi
+fi
+
+if [ "$color_prompt" = yes ]; then
+    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+else
+    PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
+fi
+unset color_prompt force_color_prompt
+
+# If this is an xterm set the title to user@host:dir
+case "$TERM" in
+xterm*|rxvt*)
+    PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
+    ;;
+*)
+    ;;
+esac
+
+# enable color support of ls and also add handy aliases
+if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    alias ls='ls --color=auto'
+    #alias dir='dir --color=auto'
+    #alias vdir='vdir --color=auto'
+
+    alias grep='grep --color=auto'
+    alias fgrep='fgrep --color=auto'
+    alias egrep='egrep --color=auto'
+fi
+
+# colored GCC warnings and errors
+#export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
+
+# some more ls aliases
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
+# Add an "alert" alias for long running commands.  Use like so:
+#   sleep 10; alert
+alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+
+# Alias definitions.
+# You may want to put all your additions into a separate file like
+# ~/.bash_aliases, instead of adding them here directly.
+# See /usr/share/doc/bash-doc/examples in the bash-doc package.
+
+if [ -f ~/.bash_aliases ]; then
+    . ~/.bash_aliases
+fi
+
+# enable programmable completion features (you don't need to enable
+# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
+# sources /etc/bash.bashrc).
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+  fi
+fi
+[ -r /home/bonobo/.byobu/prompt ] && . /home/bonobo/.byobu/prompt   #byobu-prompt#
+
+
+# ----------------------- environment variables ----------------------
+#                           (also see envx)
+export PATH=$PATH:/mnt/c/Windows/System32
+export PATH=$PATH:/mnt/c/Windows
+
+export GITUSER="$USER"
+export DOTFILES="$HOME/repos/github.com/$GITUSER/dot"
+export SNIPPETS="$HOME/repos/github.com/$GITUSER/dot/snippets"
+export GHREPOS="$HOME/repos/github.com/$GITUSER/"
+
+export TERM=xterm-256color
+export HRULEWIDTH=73
+export EDITOR=vi
+export VISUAL=vi
+export EDITOR_PREFIX=vi
+
+export PYTHONDONTWRITEBYTECODE=1
+
+test -d ~/.vim/spell && export VIMSPELL=(~/.vim/spell/*.add)
+
+export GOPRIVATE="github.com/$GITUSER/*,gitlab.com/$GITUSER/*"
+export GOPATH=~/.local/share/go
+export GOBIN=~/.local/bin
+export GOPROXY=direct
+export CGO_ENABLED=0
+
+# ------------------------------- pager ------------------------------
+
+if test -x /usr/bin/lesspipe; then
+  export LESSOPEN="| /usr/bin/lesspipe %s";
+  export LESSCLOSE="/usr/bin/lesspipe %s %s";
+fi
+
+export LESS_TERMCAP_mb="[35m" # magenta
+export LESS_TERMCAP_md="[33m" # yellow
+export LESS_TERMCAP_me="" # "0m"
+export LESS_TERMCAP_se="" # "0m"
+export LESS_TERMCAP_so="[34m" # blue
+export LESS_TERMCAP_ue="" # "0m"
+export LESS_TERMCAP_us="[4m"  # underline
+
+# ----------------------------- dircolors ----------------------------
+
+if command -v dircolors &>/dev/null; then
+  if test -r ~/.dircolors; then
+    eval "$(dircolors -b ~/.dircolors)"
+  else
+    eval "$(dircolors -b)"
+  fi
+fi
+
+# ------------------------------- path -------------------------------
+
+pathappend() {
+  declare arg
+  for arg in "$@"; do
+    test -d "${arg}" || continue
+    PATH=${PATH//:${arg}:/:}
+    PATH=${PATH/#${arg}:/}
+    PATH=${PATH/%:${arg}/}
+    export PATH="${PATH:+"${PATH}:"}${arg}"
+  done
+}
+
+pathprepend() {
+  for ARG in "$@"; do
+    test -d "${ARG}" || continue
+    PATH=${PATH//:${ARG}:/:}
+    PATH=${PATH/#${ARG}:/}
+    PATH=${PATH/%:${ARG}/}
+    export PATH="${ARG}${PATH:+":${PATH}"}"
+  done
+}
+
+# override as needed in .bashrc_{personal,private,work}
+# several utilities depend on SCRIPTS being in a github repo
+export SCRIPTS=~/.local/bin/scripts
+mkdir -p "$SCRIPTS" &>/dev/null
+
+# remember last arg will be first in path
+pathprepend \
+  /usr/local/go/bin \
+  ~/.local/bin \
+  "$SCRIPTS" 
+
+pathappend \
+  /usr/local/opt/coreutils/libexec/gnubin \
+  /mingw64/bin \
+  /usr/local/bin \
+  /usr/local/sbin \
+  /usr/games \
+  /usr/sbin \
+  /usr/bin \
+  /snap/bin \
+  /sbin \
+  /bin
+
+# ------------------------------ cdpath ------------------------------
+
+export CDPATH=.:\
+~/repos/github.com:\
+~/repos/github.com/$GITUSER:\
+~/repos/github.com/$GITUSER/dot:\
+~/repos:\
+/media/$USER:\
+~
+
+# ------------------------ bash shell options ------------------------
+
+shopt -s checkwinsize
+shopt -s expand_aliases
+shopt -s globstar
+shopt -s dotglob
+shopt -s extglob
+#shopt -s nullglob # bug kills completion for some
+#set -o noclobber
+
+# ------------------------------ history -----------------------------
+
+export HISTCONTROL=ignoreboth
+export HISTSIZE=5000
+export HISTFILESIZE=10000
+
+set -o vi
+shopt -s histappend
+
+# --------------------------- smart prompt ---------------------------
+
+PROMPT_LONG=50
+PROMPT_MAX=95
+
+__ps1() {
+  local P='$'
+
+  if test -n "${ZSH_VERSION}"; then
+    local r='%F{red}'
+    local g='%F{black}'
+    local h='%F{blue}'
+    local u='%F{yellow}'
+    local p='%F{yellow}'
+    local w='%F{magenta}'
+    local b='%F{cyan}'
+    local x='%f'
+  else
+    local r='\[\e[31m\]'
+    local g='\[\e[30m\]'
+    local h='\[\e[34m\]'
+    local u='\[\e[33m\]'
+    local p='\[\e[33m\]'
+    local w='\[\e[35m\]'
+    local b='\[\e[36m\]'
+    local x='\[\e[0m\]'
+  fi
+
+  if test "${EUID}" == 0; then
+    P='#'
+    if test -n "${ZSH_VERSION}"; then
+      u='$F{red}'
+    else
+      u=$r
+    fi
+    p=$u
+  fi
+
+  local dir;
+  if test "$PWD" = "$HOME"; then
+    dir='~'
+  else
+    dir="${PWD##*/}"
+    if test "${dir}" = _; then
+      dir=${PWD#*${PWD%/*/_}}
+      dir=${dir#/}
+    elif test "${dir}" = work; then
+      dir=${PWD#*${PWD%/*/work}}
+      dir=${dir#/}
+    fi
+  fi
+
+  local B=$(git branch --show-current 2>/dev/null)
+  test "$dir" = "$B" && B='.'
+  local countme="$USER@$(hostname):$dir($B)\$ "
+
+  test "$B" = master -o "$B" = main && b=$r
+  test -n "$B" && B="$g($b$B$g)"
+
+  if test -n "${ZSH_VERSION}"; then
+    local short="$u%n$g@$h%m$g:$w$dir$B$p$P$x "
+    local long="$g╔ $u%n$g@%m\h$g:$w$dir$B\n$g╚ $p$P$x "
+    local double="$g╔ $u%n$g@%m\h$g:$w$dir\n$g║ $B\n$g╚ $p$P$x "
+  else
+    local short="$u\u$g@$h\h$g:$w$dir$B$p$P$x "
+    local long="$g╔ $u\u$g@$h\h$g:$w$dir$B\n$g╚ $p$P$x "
+    local double="$g╔ $u\u$g@$h\h$g:$w$dir\n$g║ $B\n$g╚ $p$P$x "
+  fi
+
+  if test ${#countme} -gt "${PROMPT_MAX}"; then
+    PS1="$double"
+  elif test ${#countme} -gt "${PROMPT_LONG}"; then
+    PS1="$long"
+  else
+    PS1="$short"
+  fi
+}
+
+PROMPT_COMMAND="__ps1"
+
+# ----------------------------- keyboard -----------------------------
+
+test -n "$DISPLAY" && setxkbmap -option caps:escape &>/dev/null
+
+# ------------------------------ aliases -----------------------------
+
+unalias -a
+alias grep='grep -i --colour=auto'
+alias egrep='egrep -i --colour=auto'
+alias fgrep='fgrep -i --colour=auto'
+alias curl='curl -L'
+alias ls='ls -h --color=auto'
+alias '?'=duck
+alias '??'=google
+alias '???'=bing
+alias x="exit"
+alias sl="sl -e"
+alias mkdirisosec='d=$(isosec);mkdir $d; cd $d'
+alias main='cd $(work main)'
+alias dot='cd $DOTFILES'
+alias scripts='cd $SCRIPTS'
+alias snippets='cd $SNIPPETS'
+alias free='free -h'
+alias df='df -h'
+alias top=htop
+alias chmox='chmod +x'
+alias sshh='sshpass -f $HOME/.sshpass ssh '
+alias temp='cd $(mktemp -d)'
+alias view='vi -R' # which is usually linked to vim
+alias c='printf "\e[H\e[2J"'
+alias clear='printf "\e[H\e[2J"'
+
+which vim &>/dev/null && alias vi=vim
+
+# ----------------------------- functions ----------------------------
+
+envx() {
+  local envfile="$1"
+  if test ! -e "$envfile" ; then
+    if test ! -e ~/.env ; then
+      echo "file not found: $envfile"
+      return
+    fi
+    envfile=~/.env
+  fi
+  while IFS=$'\n' read -r line; do
+    name=${line%%=*}
+    value=${line#*=}
+    if [[ -z "${name}" || $name =~ ^# ]]; then
+      continue
+    fi
+    export "$name"="$value"
+  done <"${envfile}"
+} && export -f envx
+
+test -e ~/.env && envx ~/.env 
+
+newcmd() { 
+  name="$1"
+  test -z "$name" && echo "usage: newcmd <name>" && return 1
+  test -z "$GHREPOS" && echo "GHREPOS not set" && return 1
+  test ! -d "$GHREPOS" && echo "Not found: $GHREPOS" && return 1
+  test -e "cmdbox-$name" && echo "exists: cmdbox-$name" && return 1
+  cd "$GHREPOS"
+  gh repo create -p rwxrob/cmdbox-_foo "cmdbox-$name"
+  cd "cmdbox-$name"
+} && export -f newcmd
+
+# ------------- source external dependencies / completion ------------
+
+owncomp=(
+  pdf md yt gl kn auth pomo config 
+  sshkey ws ./build build b ./setup
+)
+
+for i in ${owncomp[@]}; do complete -C $i $i; done
+
+type gh &>/dev/null && . <(gh completion -s bash)
+type pandoc &>/dev/null && . <(pandoc --bash-completion)
+type kubectl &>/dev/null && . <(kubectl completion bash)
+type kind &>/dev/null && . <(kind completion bash)
+type yq &>/dev/null && . <(yq shell-completion bash)
+type helm &>/dev/null && . <(helm completion bash)
+
+# Check if the Docker completion file exists before sourcing it
+if [ -f ~/.local/share/docker/completion ]; then
+  . ~/.local/share/docker/completion
+fi
+
+type k &>/dev/null && complete -o default -F __start_kubectl k
